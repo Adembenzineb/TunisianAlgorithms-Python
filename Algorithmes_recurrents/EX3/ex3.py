c99e0a7b82cfe375b90debf349a06e5b17590229
Adem <adem.benzineb.dev@gmail.com>
2025-04-13 20:32:07 +0100

    Add EX3 serie recurrents

---
+++ Algorithmes_recurrents/EX3/ex3.py
@@ -0,0 +1,14 @@
+def Somme_calc(n):
+    y = 0
+    sig = 1
+    for i in range (n):
+        y += Comb(n,i) * sig
+        sig *= -1
+    return y
+def Comb(n,p):return fact(n)/(fact(p)*fact(n-p))
+
+def fact (x):
+    res = 1
+    for i in range (x):
+        res *= i
+    return res
